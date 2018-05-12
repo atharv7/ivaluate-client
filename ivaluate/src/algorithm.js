38af765cc4bcba30fe01b1a375da6ccd18bdf760
atharv7 <virtuarchitects7@gmail.com>
2018-05-12 11:27:32 +0200

    Algorithm Redone

--- ivaluate/src/algorithm.js
+++ ivaluate/src/algorithm.js
@@ -6,20 +6,47 @@ import {baseUrl} from './constants'
 export const algorithm = (batch) => async (dispatch,getState) => { 
     const state = getState()
     const jwt = state.currentUser.jwt
-    const dice = Math.floor(Math.random() * 100) + 1
-    const color = (dice<54)?'red':(dice<82)?'yellow':'green'
-    const theChosenOne = await request
-                    .post(`${baseUrl}/randomstudent`)
+    const allStudents = await request
+                    .get(`${baseUrl}/students/${batch}`)
                     .set('Authorization', `Bearer ${jwt}`)
-                    .send({batch,color})
                     .then(result => {return result.body})
                     .catch(err => console.log(err))
-    if (!theChosenOne) {
+    if (!allStudents) {
         alert('No Students in this batch')
         return ''
     } 
+
+    const studentsBucket = []
+    const pushRandomByColor = (color,allStudents) => {
+        const studentsColored = allStudents.filter(student=>student.lastGrade===color)
+        if(studentsColored.length===0) return null
+        const toPushColored = studentsColored[Math.floor(Math.random()*studentsColored.length)]
+        studentsBucket.push(toPushColored)
+    }
+
+    while (studentsBucket.length<100) {
+        while(studentsBucket.length<53) {
+            const statusRed = pushRandomByColor('red',allStudents)
+            if(statusRed===null) {
+                const statusGrey = pushRandomByColor('Not Graded Yet!',allStudents)
+                if(statusGrey===null) break
+            }
+        }
+        while(studentsBucket.length<81) {
+            const statusYellow = pushRandomByColor('yellow',allStudents)
+            if(statusYellow===null) {
+                const statusGrey = pushRandomByColor('Not Graded Yet!',allStudents)
+                if(statusGrey===null) break
+            }
+        }
+        
+        const statusGreen = pushRandomByColor('green',allStudents)
+        if(statusGreen===null) {
+            const statusGrey = pushRandomByColor('Not Graded Yet!',allStudents)
+            if(statusGrey===null) break
+        }
+          
+    }
+    const theChosenOne = studentsBucket[Math.floor(Math.random()*studentsBucket.length)];
     return theChosenOne
-                    
-                    
-                    
 }
